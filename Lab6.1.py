239b39c14ba44c90b399227d3ada23e832cda449
TankistcKz <cac.q.q6@gmail.com>
2024-11-09 12:00:37 +0300

    Add files via upload

--- Lab6.1.py
+++ Lab6.1.py
@@ -1,20 +1,25 @@
-class UserAccount():
+class UserAccount:
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
-        self.password = password
+        self.__password = password
 
+    @property
+    def password(self):
+        pass
+    
+    @password.setter
     def set_password(self, new_password):
-        self.password = new_password
+        self.__password = new_password
 
     def check_password(self, password):
-        if self.password == password:
-            return True
-        else: return False
+        return self.__password == password
 
 
 
 user = UserAccount('TankistcKz', 'gmail.com', '12345')
-user.set_password('123321')
+print(user.check_password('4'))
 print(user.check_password('12345'))
+user.set_password = '123321'
 print(user.check_password('123321'))
+print(user.__password)
